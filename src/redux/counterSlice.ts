b36deb5aff842b5f686ee96c09274f1c9ae866c2
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-05 01:09:51 +0900

    ✏️ Prettierによる誤変換を修正

--- src/redux/counterSlice.ts
+++ src/redux/counterSlice.ts
@@ -7,7 +7,7 @@ export const counterSlice = createSlice({
   },
   reducers: {
     increment: (state) => {
-      state.value = +1;
+      state.value += 1;
     },
     decrement: (state) => {
       state.value -= 1;
